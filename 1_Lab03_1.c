201a5e8de7d5fc343311d2b727079f344d675e33
lvhyuni <psihyun0225@naver.com>
2023-05-09 08:02:47 +0900

    1_Lab03

---
+++ 1_Lab03_1.c
@@ -0,0 +1,14 @@
+int main(void){
+    int A, B;
+    scanf("%d %d", &A, &B);
+ 
+    if(A>B){
+        printf(">");
+    }
+    else if(A<B){
+        printf("<");
+    }
+    else{
+        printf("==");
+    }
+}
