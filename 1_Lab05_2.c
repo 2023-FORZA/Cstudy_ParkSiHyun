c77db49648e98dbfb6b5b9b15df3c7a897ffca7f
lvhyuni <psihyun0225@naver.com>
2023-05-22 19:23:08 +0900

    1_Lab05

---
+++ 1_Lab05_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n, max=0, number=0;
+	for(int i = 0; i < 9; i++)
+    {
+        scanf("%d", &n);
+		if (n>max)
+		{
+			max = n;
+			number = i+1;
+		}
+    }
+			
+	printf("%d\n%d", max, number);
+    
+}
