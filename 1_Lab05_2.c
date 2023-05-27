4fdd3fcf8e469d8dd1b9599455087596840cb45a
lvhyuni <psihyun0225@naver.com>
2023-05-27 21:34:05 +0900

    1_Lab05

--- 1_Lab05_2.c
+++ 1_Lab05_2.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-int main()
-{
-	int n, max=0, number=0;
-	for(int i = 0; i < 9; i++)
-    {
-        scanf("%d", &n);
-		if (n>max)
-		{
-			max = n;
-			number = i+1;
-		}
-    }
-			
-	printf("%d\n%d", max, number);
+int main(){
+    int n, a[100], v, count = 0;
+    scanf("%d", &n);
     
+    for (int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    
+    scanf("%d", &v);
+    
+    for (int j = 0; j < n; j++){
+        if (a[j] == v)
+            count++;
+    }
+    
+    printf("%d", count);
 }
