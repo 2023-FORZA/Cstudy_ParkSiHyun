93652f9fdb0dd66918c18c83be4190a3f089e659
lvhyuni <psihyun0225@naver.com>
2023-08-31 14:27:23 +0900

    SUMMER_Lab04

---
+++ SUMMER_Lab04_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main() {
+    int coin[] = { 500,100,50,10,5,1 };
+    int money, i = 0, cnt = 0;
+    scanf("%d", &money);
+ 
+    money = 1000 - money;
+    while (money != 0) {
+        if (coin[i] > money) {
+            i++;
+            continue;
+        }
+        else {
+            money -= coin[i];
+            cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
